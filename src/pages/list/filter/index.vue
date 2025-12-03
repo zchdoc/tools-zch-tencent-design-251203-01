f8330b73a9b83ee123b443f7df95cccc9688c258
zchcpy <571928489@qq.com>
2025-12-03 18:10:04 +0800

    first commit

---
+++ src/pages/list/filter/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <common-table />
+</template>
+<script setup lang="ts">
+import CommonTable from '@/components/common-table/index.vue';
+
+defineOptions({
+  name: 'ListFilter',
+});
+</script>
